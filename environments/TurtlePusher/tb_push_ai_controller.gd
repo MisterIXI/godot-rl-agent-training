8f47457c5c0ac5e760249fc7f8f71ed7deb50ab0
Yannik Braendle <y.braendle@th-bingen.de>
2025-01-21 12:58:28 +0100

    tb raycast push tests

--- environments/TurtlePusher/tb_push_ai_controller.gd
+++ environments/TurtlePusher/tb_push_ai_controller.gd
@@ -3,6 +3,7 @@ class_name TBPushAIController
 @onready var turtle: TurtleBotController = get_parent()
 @onready var env: TurtlePusherEnv = get_parent().get_parent()
 @export var info_label: Label3D
+@export var raycast_sens: RayCastSensor3D
 
 var running_rewards: Array[float] = []
 var is_success: bool = false
@@ -11,27 +12,32 @@ var closest_dist: float = 0
 var closest_to_ball: float = 0
 var ball_in_button_area: bool = false
 var has_been_close_to_ball: bool = false
-# func _ready():
-# 	pass
+
 var turns_made: int = 0
 var angle_accum: float = 0
 
 func get_obs() -> Dictionary:
+	var sens = raycast_sens.get_observation()
+	# for i in range(sens.size()):
+	# 	sens[i] = 1 if sens[i] > 0 else 0
+	raycast_sens.display()
 	var factor = env.settings.env_size.x
 	var ball_pos = turtle.to_local(env.ball.global_position)
 	var target_pos = turtle.to_local(env.target.global_position)
 	# ball vel in turtle local space, scaled by 10 to be roughly -1 to 1
 	var ball_vel = turtle.to_local(env.ball.linear_velocity + turtle.global_position)
-	return {"obs": [
+	var obs_arr = [
 		turtle.position.x / factor,
 		turtle.position.z / factor,
 		target_pos.x / factor,
 		target_pos.z / factor,
-		ball_pos.x / factor,
-		ball_pos.z / factor,
-		ball_vel.x * 10,
-		ball_vel.z * 10,
-	]}
+		# ball_pos.x / factor,
+		# ball_pos.z / factor,
+		# ball_vel.x * 10,
+		# ball_vel.z * 10,
+	]
+	obs_arr.append_array(sens)
+	return {"obs": obs_arr}
 
 
 func get_reward() -> float:
@@ -64,7 +70,7 @@ func get_action_space() -> Dictionary:
 
 
 func set_action(action) -> void:
-	turtle.set_twist_vel(clamp(action["twist_vel"][0], -1.0, 0.8))
+	turtle.set_twist_vel(clamp(action["twist_vel"][0], -1.0, 0.5))
 	# turtle.set_twist_vel(-action["twist_vel"][0])
 	turtle.set_twist_ang(action["twist_ang"][0])
 
